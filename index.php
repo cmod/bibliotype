9284c52c119f7dafed0bb94c3552c1a008eaa746
Craig Mod <stuff@craigmod.com>
2010-12-26 10:59:31 -0500

    first commit -- pre-polish

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Longform Base Typography for Tablets</title>
+
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+
+  <link href="css/master.css" rel="stylesheet" type="text/css" />
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
+  <script type="text/javascript" src="js/jquery.doubletap.js"></script> 
+  <script type="text/javascript" src="js/Hyphenator.js"></script> 
+  <script type="text/javascript" src="js/bibliotype.js"></script>
+
+</head>
+
+<body class="low georgia">
+
+  <div id="menu_wrap">
+    <menu>
+
+      <ul class="sizes">
+        <li class="desc">Size</li>
+        <li class="size1">bed</li>
+        <li class="size3">knee</li>
+        <li class="size5">breakfast</li>
+      </ul>
+
+      <ul class="text_type">
+        <li class="desc">Justification</li>
+        <li class="just">Justified</li>
+        <li class="rag">Ragged</li>
+      </ul>
+
+      <ul class="family">
+        <li class="desc">Family</li>
+        <li class="hel">Helvetica</li>
+        <li class="geo">Georgia</li>
+      </ul>
+      
+      <ul class="contrast">
+        <li class="desc">Contrast</li>
+        <li class="highc">High</li>
+        <li class="lowc">Low</li>
+      </ul>
+      
+      <ul class="grid">
+        <li class="desc">Grid</li>
+        <li class="bg_on">Grid On</li>
+        <li class="bg_off">Grid Off</li>
+      </ul>
+      
+      <ul class="hyphenation">
+        <li class="desc">Hyphenation</li>
+        <li id="hyphenToggle">Toggle</li>
+      </ul>
+    </menu>
+  </div>
+
+  <div id="content_container" class="knee">
+
+    <!--div class="not-tablet">note: These templates are optimized for viewing on the iPad</div-->
+    <header>Flatland: A Romance of Many Dimensions</header>
+    <div class="body_text">
+      <div id="title_block">
+        <h2>SECTION 15</h2>
+        <h3>Concerning a Stranger from Spaceland</h3>
+      </div>
+      
+      <p class="fp"><span class="sc">From dreams</span> I proceed to facts.</p>
+
+      <p>It was the last day of our 1999<sup>th</sup> year of our era.  The patterning of
+      the rain had long ago announced nightfall; and I was sitting <em>(footnote
+      3)</em> in the company of my wife, musing on the events of the past and the
+      prospects of the coming year, the coming century, the coming Millennium.</p>
+
+      <p>My four Sons and two orphan Grandchildren had retired to their several
+      apartments; and my wife alone remained with me to see the old
+      Millennium out and the new one in.</p>
+      
+      <blockquote>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Fdunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </blockquote>
+      
+      <p>I was rapt in thought, pondering in my mind some words that had
+      casually issued from the mouth of my youngest Grandson, a most
+      promising young Hexagon of unusual brilliancy and perfect angularity.
+      His uncles and I had been giving him his usual practical lesson in
+      Sight Recognition, turning ourselves upon our centres, now rapidly, now
+      more slowly, and questioning him as to our positions; and his answers
+      had been so satisfactory that I had been induced to reward him by
+      giving him a few hints on Arithmetic, as applied to Geometry.</p>
+      
+      <p>Taking nine Squares, each an inch every way, I had put them together so
+      as to make one large Square, with a side of three inches, and I had
+      hence proved to my little Grandson that&mdash;though it was impossible for
+      us to SEE the inside of the Square&mdash;yet we might ascertain the number
+      of square inches in a Square by simply squaring the number of inches in
+      the side:  "and thus," said I, "we know that three-to-the-second, or
+      nine, represents the number of square inches in a Square whose side is
+      three inches long."</p>
+      
+      <p>The little Hexagon meditated on this a while and then said to me; "But
+      you have been teaching me to raise numbers to the third power: I
+      suppose three-to-the-third must mean something in Geometry; what does
+      it mean?"  "Nothing at all," replied I, "not at least in Geometry; for
+      Geometry has only Two Dimensions."  And then I began to shew the boy
+      how a Point by moving through a length of three inches makes a Line of
+      three inches, which may be represented by three; and how a Line of
+      three inches, moving parallel to itself through a length of three
+      inches, makes a Square of three inches every way, which may be
+      represented by three-to-the-second.  xxx Upon this, my Grandson, again
+      returning to his former suggestion, took me up rather suddenly and
+      exclaimed, "Well, then, if a Point by moving three inches, makes a Line
+      of three inches represented by three; and if a straight Line of three
+      inches, moving parallel to itself, makes a Square of three inches every
+      way, represented by three-to-the-second; it must be that a Square of
+      three inches every way, moving somehow parallel to itself (but I don't
+      see how) must make Something else (but I don't see what) of three
+      inches every way&mdash;and this must be represented by three-to-the-third."</p>
+      
+      <p>"Go to bed," said I, a little ruffled by this interruption: "if you
+      would talk less nonsense, you would remember more sense."</p>
+      
+      <p>So my Grandson had disappeared in disgrace; and there I sat by my
+      Wife's side, endeavouring to form a retrospect of the year 1999 and of
+      the possibilities of the year 2000; but not quite able to shake of the
+      thoughts suggested by the prattle of my bright little Hexagon.  Only a
+      few sands now remained in the half-hour glass.  Rousing myself from my
+      reverie I turned the glass Northward for the last time in the old
+      Millennium; and in the act, I exclaimed aloud, "The boy is a fool."</p>
+      
+      <p>Straightway I became conscious of a Presence in the room, and a
+      chilling breath thrilled through my very being.  "He is no such thing,"
+      cried my Wife, "and you are breaking the Commandments in thus
+      dishonouring your own Grandson."  But I took no notice of her.  Looking
+      around in every direction I could see nothing; yet still I FELT a
+      Presence, and shivered as the cold whisper came again.  I started up.
+      "What is the matter?" said my Wife, "there is no draught; what are you
+      looking for?  There is nothing."  There was nothing; and I resumed my
+      seat, again exclaiming, "The boy is a fool, I say; three-to-the-third
+      can have no meaning in Geometry." At once there came a distinctly
+      audible reply, "The boy is not a fool; and three-to-the-third has an
+      obvious Geometrical meaning."</p>
+      
+      <p>My Wife as well as myself heard the words, although she did not
+      understand their meaning, and both of us sprang forward in the
+      direction of the sound.  What was our horror when we saw before us a
+      Figure!  At the first glance it appeared to be a Woman, seen sideways;
+      but a moment's observation shewed me that the extremities passed into
+      dimness too rapidly to represent one of the Female Sex; and I should
+      have thought it a Circle, only that it seemed to change its size in a
+      manner impossible for a Circle or for any regular Figure of which I had
+      had experience.</p>
+      
+      <p>But my Wife had not my experience, nor the coolness necessary to note
+      these characteristics.  With the usual hastiness and unreasoning
+      jealousy of her Sex, she flew at once to the conclusion that a Woman
+      had entered the house through some small aperture.  "How comes this
+      person here?" she exclaimed, "you promised me, my dear, that there
+      should be no ventilators in our new house."  "Nor are they any," said
+      I; "but what makes you think that the stranger is a Woman?  I see by my
+      power of Sight Recognition&mdash;"</p>
+      
+      <p>"Oh, I have no patience with your Sight Recognition," replied she,
+      "'Feeling is believing' and 'A Straight Line to the touch is worth a
+      Circle to the sight'"&mdash;two Proverbs, very common with the Frailer Sex
+      in Flatland.</p>
+      
+      <p>"Well," said I, for I was afraid of irritating her, "if it must be so,
+      demand an introduction."  Assuming her most gracious manner, my Wife
+      advanced towards the Stranger, "Permit me, Madam to feel and be felt
+      by&mdash;" then, suddenly recoiling, "Oh! it is not a Woman, and there are
+      no angles either, not a trace of one.  Can it be that I have so
+      misbehaved to a perfect Circle?"</p>
+      
+      <p>"I am indeed, in a certain sense a Circle," replied the Voice, "and a
+      more perfect Circle than any in Flatland; but to speak more accurately,
+      I am many Circles in one."  Then he added more mildly, "I have a
+      message, dear Madam, to your husband, which I must not deliver in your
+      presence; and, if you would suffer us to retire for a few minutes&mdash;"
+      But my wife would not listen to the proposal that our august Visitor
+      should so incommode himself, and assuring the Circle that the hour of
+      her own retirement had long passed, with many reiterated apologies for
+      her recent indiscretion, she at last retreated to her apartment.</p>
+      
+      <p>I glanced at the half-hour glass.  The last sands had fallen.  The
+      third Millennium had begun.</p>
+
+      <p class="closing">*</p> 
+
+      <div class="footnotes">
+        <ul>
+          <li>
+            <p>Footnote 3.  When I say "sitting," of course I do not mean any change
+            of attitude such as you in Spaceland signify by that word; for as we
+            have no feet, we can no more "sit" nor "stand" (in your sense of the
+            word) than one of your soles or flounders.</p>
+            
+            <p>Nevertheless, we perfectly well recognize the different mental states
+            of volition implied by "lying," "sitting," and "standing," which are to
+            some extent indicated to a beholder by a slight increase of lustre
+            corresponding to the increase of volition.</p>
+            
+            <p>But on this, and a thousand other kindred subjects, time forbids me to
+            dwell.</p>
+          </li>
+          <li>
+            <p>Another footnote!</p>
+            <p>This is where the text for a secon footnote would fall.</p>
+          </li>
+        </ul>
+      </div>
+    
+    </div>
+    
+    <footer>Edwin Abbott Abbott</footer>
+      
+  </div>
+
+</body>
+
+</html>
---
+++ js/bibliotype.js
@@ -0,0 +1,118 @@
+/*  ---------------------------------------------------
+
+    Bibliotype Javascript handlers
+
+    --------------------------------------------------- */
+
+
+    $(document).ready(function() {
+      
+      Hyphenator.config({
+              displaytogglebox : true,
+              classname: 'body_text',
+              minwordlength : 4,
+              togglebox : function (hyphenate) {
+                        var myelem = document.getElementById('hyphenToggle');
+                        if (hyphenate) {
+                          myelem.style.backgroundColor = '#000000';
+                          myelem.onclick = Hyphenator.toggleHyphenation;
+                        } else {
+                          myelem.style.backgroundColor = '#ffffff';
+                          myelem.onclick = Hyphenator.toggleHyphenation;
+                        }
+                }
+      });
+      Hyphenator.run();
+      
+      $('body').addSwipeEvents().
+        bind('doubletap', function(evt, touch) {
+          $('menu').slideToggle('fast', function() {
+            // Animation complete.
+          });
+      });
+
+      $(document).bind('dblclick', function(){
+        $('menu').slideToggle('fast', function() {
+            // Animation complete.
+          });
+      });
+
+      // -------------------------------------
+      // Size toggles
+      //
+      $('.size1').click(function() {
+        $('#content_container').removeClass();
+        $('#content_container').addClass('bed');
+      });
+
+      $('.size3').click(function() {
+        $('#content_container').removeClass();
+        $('#content_container').addClass('knee');
+      });
+
+      $('.size5').click(function() {
+        $('#content_container').removeClass();
+        $('#content_container').addClass('breakfast');
+      });
+      
+      
+      // -------------------------------------
+      // Contrast Toggles
+      //
+      $('.highc').click(function() {
+        $('body').removeClass('low');
+        $('body').removeClass('high');
+        $('body').addClass('high');
+      });
+
+      $('.lowc').click(function() {
+        $('body').removeClass('low');
+        $('body').removeClass('high');
+        $('body').addClass('low');
+      });
+
+
+      // -------------------------------------
+      // Justification Toggles
+      //
+      $('.just').click(function() {
+        $('body').removeClass('ragged');
+        $('body').removeClass('justified');
+        $('body').addClass('justified');
+      });
+
+      $('.rag').click(function() {
+        $('body').removeClass('ragged');
+        $('body').removeClass('justified');
+        $('body').addClass('ragged');
+      });
+
+
+      // -------------------------------------
+      // Serif toggle
+      //
+      $('.hel').click(function() {
+        $('body').removeClass('helvetica');
+        $('body').removeClass('georgia');
+        $('body').addClass('helvetica');
+      });
+
+      $('.geo').click(function() {
+        $('body').removeClass('helvetica');
+        $('body').removeClass('georgia');
+        $('body').addClass('georgia');
+      });
+
+
+      $('.bg_on').click(function() {
+        $('body').removeClass('bg_grid');
+        $('body').addClass('bg_grid');
+      });
+
+      $('.bg_off').click(function() {
+        $('body').removeClass('bg_grid');
+      });
+
+
+
+    })
